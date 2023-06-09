0ffec8095148355fc43a0687d2d83394f4c26559
Carsolarte <carsolarte@unicauca.edu.co>
2023-06-08 23:05:30 -0500

    Se crearon metodos GET POST DELETE en el servidor

--- servidor/src/routes/usuariosRoutes.ts
+++ servidor/src/routes/usuariosRoutes.ts
@@ -9,7 +9,12 @@ class UsuariosRoutes{
 
     }
     config():void{
-        this.router.get('/',usuariosControllers.index);
+        this.router.get('/',usuariosControllers.list);
+        this.router.get('/',usuariosControllers.getOne);
+        this.router.post('/',usuariosControllers.create);
+        this.router.delete('/:id',usuariosControllers.delete);
+        this.router.put('/:id',usuariosControllers.update);
+
     }
 }
 const usuariosRoutes=new UsuariosRoutes();
