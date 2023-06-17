1c977b3e34339967def4440b29e8975a7ffdce32
Daniela <driascos@unicauca.edu.co>
2023-06-16 22:39:37 -0500

    queda la mitad de la implementacion de editar usuario

--- cliente/src/app/app-routing.module.ts
+++ cliente/src/app/app-routing.module.ts
@@ -8,14 +8,14 @@ import { LaborDocenteListarComponent } from './componets/labor-docente-listar/la
 import { UsuarioListarComponent } from './componets/usuario-listar/usuario-listar.component';
 import { UsuarioAgregarComponent } from './componets/usuario-agregar/usuario-agregar.component';
 import { MenuCoordinadorComponent } from './componets/menu-coordinador/menu-coordinador.component';
-
+import {UsuarioEditarComponent} from './componets/usuario-editar/usuario-editar.component';
 
 const routes: Routes = [
   { path: 'login', component: LoginComponent },
   { path: 'menuCoordinador',component : MenuCoordinadorComponent },
   { path: 'listar', component: UsuarioListarComponent },
   { path: 'agregar', component: UsuarioAgregarComponent },
-  { path: 'editar/:id',component: UsuarioAgregarComponent},
+  { path: 'editar/:id',component: UsuarioEditarComponent},
   { path: 'listarL', component: LaborDocenteListarComponent },
   { path: 'agregarL', component: LaborDocenteAgregarComponent },
   { path: 'editarL/:id',component: LaborDocenteAgregarComponent},
--- cliente/src/app/componets/usuario-editar/usuario-editar.component.ts
+++ cliente/src/app/componets/usuario-editar/usuario-editar.component.ts
@@ -1,10 +1,56 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, HostBinding} from '@angular/core';
+import { Usuario } from '../../Modelo/Usuario';
+import { UseRol} from '../../Modelo/UseRol';
+import { ActivatedRoute, Router ,Route} from '@angular/router';
+import { ServiceService } from '../../Service/service.service';
 
 @Component({
   selector: 'app-usuario-editar',
   templateUrl: './usuario-editar.component.html',
   styleUrls: ['./usuario-editar.component.css']
 })
-export class UsuarioEditarComponent {
-
+export class UsuarioEditarComponent implements OnInit {
+   @HostBinding('class')classes= 'row'
+  
+  
+    usuario: Usuario & UseRol = {
+      USR_IDENTIFICACION: 0,
+      USU_NOMBRE: '',
+      USU_APELLIDO: '',
+      USU_GENERO: '',
+      USU_ESTUDIO: '',
+      UserName: '',
+      USR_Contrasenia: '',
+      UR_FECHAINICIO: new Date(),
+      UR_FECHAFIN: new Date(),
+      ROL_ID: 0,
+    };
+    edit : boolean =false;
+    constructor(private serviceService: ServiceService,private router:Router,private activeRouter:ActivatedRoute){
+      
+    }
+    ngOnInit(): void {
+      
+      const params = this.activeRouter.snapshot.params;
+      if (params['id']) {
+        this.serviceService.getUsuario(params['id']).subscribe(
+          res => {
+            console.log(res);
+            this.usuario=res as Usuario&UseRol ;
+            console.log(this.usuario)
+            this.edit = true;
+          },
+          err => console.error(err)
+        );
+      }
+    }
+    
+    
+    obtenerId(){
+      return this.usuario.USR_IDENTIFICACION;
+    }
+    updateUsuario(){
+      console.log(this.usuario.USR_IDENTIFICACION);
+      this.serviceService.updateUsuario(this.usuario.USR_IDENTIFICACION,this.usuario);
+    }
 }
