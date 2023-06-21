a8fceb36f435607c6d1a220199a7d6fd9215c0bc
Carsolarte <carsolarte@unicauca.edu.co>
2023-06-21 16:49:22 -0500

    Middleware

--- cliente/src/app/componets/middleware/middleware.component.ts
+++ cliente/src/app/componets/middleware/middleware.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { ActivatedRoute,Router } from '@angular/router';
+
+import { ServiceService } from '../../Service/service.service';
 
 @Component({
   selector: 'app-middleware',
@@ -6,5 +9,19 @@ import { Component } from '@angular/core';
   styleUrls: ['./middleware.component.css']
 })
 export class MiddlewareComponent {
+
+
+  constructor(private serviceService: ServiceService,
+    private router: Router,
+    private activeRouter: ActivatedRoute)
+  {}
+
+  verificarVistaMenuPrincipal(rol:number,userId:number){
+    if(rol==2){
+      this.router.navigate([`/menuCoordinador/${userId}`]);
+    }else if(rol==3 || rol==4 || rol==5){
+      this.router.navigate([`/docente/${userId}`]);
+      }
+  }
 
 }
