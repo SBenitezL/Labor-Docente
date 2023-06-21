5bf2408399bebc0e72b6dc9acb93abf620118783
Carsolarte <carsolarte@unicauca.edu.co>
2023-06-21 02:03:32 -0500

    Cambio html Docente

--- cliente/src/app/componets/docente/docente.component.ts
+++ cliente/src/app/componets/docente/docente.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { ServiceService } from 'src/app/Service/service.service';
 import { Evaluacion } from 'src/app/Modelo/Evaluacion';
+import { Usuario } from 'src/app/Modelo/Usuario';
 
 @Component({
   selector: 'app-docente',
@@ -10,25 +11,46 @@ import { Evaluacion } from 'src/app/Modelo/Evaluacion';
 })
 export class DocenteComponent implements OnInit {
   evaluaciones: Evaluacion[] = [];
+  usuario: Usuario  = {
+    USR_IDENTIFICACION: 0,
+    USU_NOMBRE: '',
+    USU_APELLIDO: '',
+    USU_GENERO: '',
+    USU_ESTUDIO: '',
+    UserName: '',
+    USR_Contrasenia: '' 
+  };
+
 
   constructor(private activeRouter: ActivatedRoute, private serviceService: ServiceService, private router: Router) { }
 
   ngOnInit() {
     this.getEvaluacion();
   }
-
   getEvaluacion() {
     const id = this.activeRouter.snapshot.params['id'];
-    if (id) {
-      this.serviceService.getEvaluacion(id).subscribe(
+    const per_id = this.activeRouter.snapshot.params['per_id'];
+    if (id && per_id) {
+      console.log("Entro");
+      this.serviceService.getEvaluacion(id, per_id).subscribe(
         (res: any) => {
           console.log(res);
           this.evaluaciones = res;
         },
         err => console.error(err)
       );
+  
+      // Obtener información del usuario
+      this.serviceService.getUsuario(id).subscribe(
+        (res: any) => {
+          console.log(res);
+          this.usuario = res;
+        },
+        err => console.error(err)
+      );
     }
   }
-}
+  
+}  
 
 
