ce1a53391999b42db66f9e8078096c86ae02ea78
Carsolarte <carsolarte@unicauca.edu.co>
2023-06-20 20:52:39 -0500

    Creacion de componente Docente

--- cliente/src/app/app.module.ts
+++ cliente/src/app/app.module.ts
@@ -22,6 +22,7 @@ import { LaborDocenteEditarComponent } from './componets/labor-docente-editar/la
 import { MenuCoordinadorComponent } from './componets/menu-coordinador/menu-coordinador.component';
 import { EvaluacionListComponent } from './componets/evaluacion-list/evaluacion-list.component';
 import { EvaluacionFormComponent } from './componets/evaluacion-form/evaluacion-form.component';
+import { DocenteComponent } from './componets/docente/docente.component';
 
 @NgModule({
   declarations: [
@@ -36,7 +37,8 @@ import { EvaluacionFormComponent } from './componets/evaluacion-form/evaluacion-
     LaborDocenteEditarComponent,
     MenuCoordinadorComponent,
     EvaluacionListComponent,
-    EvaluacionFormComponent
+    EvaluacionFormComponent,
+    DocenteComponent
   ],
   imports: [
     BrowserModule,
